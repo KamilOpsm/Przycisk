e445371a748a9f03389c0d75277a5a34257da97d
Kamil <kamil.opsm@wp.pl>
2022-08-02 22:23:01 +0200

    end project

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Nic tu nie ma 😱')
+// const btn = document.querySelector('.btn');
+const btns = document.querySelectorAll('.btn');
+
+const btnAnimation = (e) => {
+	const top = e.clientY;
+	const left = e.clientX;
+
+	//pozycja, w którą klikamy
+
+	const btnTopPosition = e.target.offsetTop;
+	const btnLeftPosition = e.target.offsetLeft;
+
+	//pozycja przycisku
+
+	const insideBtnTop = top - btnTopPosition;
+	const insideBtnLeft = left - btnLeftPosition;
+
+	const circle = document.createElement('span');
+	circle.classList.add('circle');
+	circle.style.top = insideBtnTop + 'px';
+	circle.style.left = insideBtnLeft + 'px';
+
+	e.target.appendChild(cirscle);
+
+	setTimeout(() => {
+		circle.remove();
+	}, 300);
+};
+
+// btn.addEventListener('click', btnAnimation);
+
+btns.forEach(btn => btn.addEventListener('click', btnAnimation));
